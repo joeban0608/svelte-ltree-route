05c6985e6af14a6cb2e6a08faa437fcd789f88ae
hongbangzhou <joeanddaneil5@gmail.com>
2025-08-01 01:54:56 +0800

    fix: layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import classList from '$lib/action/classList';
+	import { Navbar, NavBrand, NavHamburger, NavUl, DarkMode, NavLi } from 'flowbite-svelte';
 	import '../app.css';
+	import type { Snippet, SvelteComponent } from 'svelte';
 
-	let { children } = $props();
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
+
 <svelte:body
 	use:classList={[
 		'bg-white',
@@ -14,4 +21,31 @@
 		'dark:text-white'
 	]}
 />
-{@render children()}
+
+<Navbar class="fixed top-0 z-50 w-full">
+	<div class="flex gap-4">
+		<NavBrand href="/">
+			<img
+				src="https://flowbite.com/docs/images/logo.svg"
+				class="mr-3 h-6 sm:h-9"
+				alt="Flowbite Logo"
+			/>
+			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+				Flowbite
+			</span>
+		</NavBrand>
+		<DarkMode class="border text-primary-500 dark:border-gray-800 dark:text-primary-600" />
+	</div>
+
+	<NavHamburger />
+	<NavUl>
+		<NavLi
+			href="/"
+			activeClass="text-blue-700 dark:text-white"
+			nonActiveClass="text-gray-700 dark:text-gray-400">Home</NavLi
+		>
+	</NavUl>
+</Navbar>
+<div class="h-screen pt-16 md:pt-20">
+	{@render children()}
+</div>
