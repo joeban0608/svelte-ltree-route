9890471a3975415688968e4b4cedd51f1170e077
hongbangzhou <joeanddaneil5@gmail.com>
2025-08-01 02:21:45 +0800

    sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,49 @@
 <script lang="ts">
 	import classList from '$lib/action/classList';
-	import { Navbar, NavBrand, NavHamburger, NavUl, DarkMode, NavLi } from 'flowbite-svelte';
+	import {
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavUl,
+		DarkMode,
+		NavLi,
+		Sidebar,
+		SidebarDropdownWrapper,
+		SidebarGroup,
+		SidebarItem,
+		uiHelpers,
+		SidebarButton,
+
+		Button
+
+	} from 'flowbite-svelte';
 	import '../app.css';
-	import type { Snippet, SvelteComponent } from 'svelte';
+	import {
+		ArrowRightToBracketOutline,
+		ChartOutline,
+		EditSolid,
+		GridSolid,
+		MailBoxSolid,
+		ShoppingBagSolid,
+		UserSolid
+	} from 'flowbite-svelte-icons';
+
+	import { page } from '$app/state';
+	import type { Snippet } from 'svelte';
 
 	interface Props {
 		children: Snippet;
 	}
 
 	let { children }: Props = $props();
+
+	const spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	const demoSidebarUi = uiHelpers();
+	let isDemoOpen = $state(false);
+	const closeDemoSidebar = demoSidebarUi.close;
+	$effect(() => {
+		isDemoOpen = demoSidebarUi.isOpen;
+	});
 </script>
 
 <svelte:body
@@ -46,6 +81,31 @@
 		>
 	</NavUl>
 </Navbar>
-<div class="h-screen pt-16 md:pt-20">
-	{@render children()}
+
+<SidebarButton
+	onclick={demoSidebarUi.toggle}
+	class="fixed top-14 !bg-gray-800 p-2"
+	style="z-index:99"
+/>
+<div class="relative h-screen pt-24 md:pt-20">
+	<Sidebar
+		isSingle={false}
+		backdrop={false}
+		isOpen={isDemoOpen}
+		closeSidebar={closeDemoSidebar}
+		params={{ x: -50, duration: 50 }}
+		position="absolute"
+		activeClass="p-2"
+		nonActiveClass="p-2"
+		class="top-auto bottom-0 z-50 h-[calc(100vh-3.5rem)] pt-12 md:h-[calc(100vh-5rem)]"
+	>
+		<SidebarGroup>
+			<Button>
+				+ Create
+			</Button>
+		</SidebarGroup>
+	</Sidebar>
+	<main class="z-0 h-full md:pl-64">
+		{@render children()}
+	</main>
 </div>
