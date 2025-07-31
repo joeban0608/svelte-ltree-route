bc6fd7959bc97e8399039700c6c24833bbc1539b
hongbangzhou <joeanddaneil5@gmail.com>
2025-08-01 01:42:43 +0800

    - add ui flowbite - add svelte/action classList - add example header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import classList from '$lib/action/classList';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
+<svelte:body
+	use:classList={[
+		'bg-white',
+		'text-black',
+		'dark:bg-gradient-to-r',
+		'dark:from-purple-900',
+		'dark:to-gray-900',
+		'dark:text-white'
+	]}
+/>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
+</script>
+
+<DarkMode class="border text-primary-500 dark:border-gray-800 dark:text-primary-600" />
+
+<Navbar>
+	<NavBrand href="/">
+		<img
+			src="https://flowbite.com/docs/images/logo.svg"
+			class="mr-3 h-6 sm:h-9"
+			alt="Flowbite Logo"
+		/>
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+			Flowbite
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi
+			href="/"
+			activeClass="text-blue-700 dark:text-white"
+			nonActiveClass="text-gray-700 dark:text-gray-400">Home</NavLi
+		>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/services">Services</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
