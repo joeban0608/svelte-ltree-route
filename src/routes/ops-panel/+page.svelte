4b676c7b68cb033a006ecaeb81a10754ff532152
hongbangzhou <joeanddaneil5@gmail.com>
2025-08-03 02:19:38 +0800

    Ops panel page

--- src/routes/ops-panel/+page.svelte
+++ src/routes/ops-panel/+page.svelte
@@ -6,4 +6,6 @@
 	let { data }: Props = $props();
 </script>
 
-<h1>Ops Panel</h1>
+<main class="z-0 m-auto flex h-full flex-col items-center justify-start gap-4 overflow-y-scroll py-4">
+	<h1>Ops Panel</h1>
+</main>
